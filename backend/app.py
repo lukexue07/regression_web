a4026e3c8cb5b386173ea3d2e94843094788bcba
lukexue07 <lxue1@ufl.edu>
2025-06-20 23:18:44 -0400

    adjusted it to work for render.com

--- backend/app.py
+++ backend/app.py
@@ -85,6 +85,6 @@ def regress():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    #port = int(os.environ.get('PORT', 5000))
-    #app.run(host = '0.0.0.0', port= port, debug=True) 
-    app.run(debug=True) #only have this line for local testing
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host = '0.0.0.0', port= port, debug=True) 
+    #app.run(debug=True) #only have this line for local testing
